113b0643cacedb452714e82a70162783a8476705
Roy C <v0o0605@gmail.com>
2022-12-13 11:40:50 +0900

    design pattern 중 singleton pattern

--- src/main/java/com/personal/spring_prac/design_pattern/singleton/SocketClient.java
+++ src/main/java/com/personal/spring_prac/design_pattern/singleton/SocketClient.java
@@ -7,13 +7,14 @@ public class SocketClient {
 
     private static SocketClient socketClient = null;
 
-    private SocketClient(){     // default 생성자를 private 으로 설정해 다른곳에서 새로이 객체를 생성하지 못하게 함 (최초에 한번만 생성되도록)
+    private SocketClient() {     // default 생성자를 private 으로 설정해 다른곳에서 새로이 객체를 생성하지 못하게 함 (최초에 한번만 생성되도록)
     }
 
     public static SocketClient getInstance() {      // static 메서드를 통해서 자신의 객체를 제공한다.
-
-        if (socketClient == null) {
-            socketClient = new SocketClient();
+        synchronized (SocketClient.class) {
+            if (socketClient == null) {
+                socketClient = new SocketClient();
+            }
         }
         return socketClient;
     }

5a51ad78c155281b173b4bbb3f557bce1b9a0ee7
Roy C <v0o0605@gmail.com>
2022-12-17 23:59:01 +0900

    REST API (DELETE)

---
+++ src/main/java/com/personal/spring_prac/controller/DeleteController.java
@@ -0,0 +1,16 @@
+package com.personal.spring_prac.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class DeleteController {
+
+    // Delete 는 실행 후 항상 삭제된 상태 이기 때문에 멱등성 유지
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable(name = "userId") Long id, @RequestParam String account) {
+
+        System.out.println(id);
+        System.out.println(account);
+    }
+}

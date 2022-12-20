59ef8ce32ef2f5788975764bd648ef41a03ae740
Roy C <v0o0605@gmail.com>
2022-12-20 23:11:44 +0900

    IOC / DI

--- src/main/java/com/personal/spring_prac/ioc/di/Main.java
+++ src/main/java/com/personal/spring_prac/ioc/di/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         String url = "www.google.com/books/it?page=10&size=20&name=spring-boot";
 
-        Encoder encoder = new Encoder();
+        Encoder encoder = new Encoder(new Base64Encoder());     // Encoder 내부를 건드리지 않고 Base64Encoder 를 외부에서 주입을 받아 사용
         String result = encoder.encode(url);
         System.out.println(result);
     }

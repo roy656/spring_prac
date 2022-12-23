4f5c089b761338ace65da898355d2dee3ee24f7f
Roy C <v0o0605@gmail.com>
2022-12-23 21:40:11 +0900

    SpringValidation

---
+++ src/main/java/com/personal/spring_prac/validation/controller/ValController.java
@@ -0,0 +1,37 @@
+package com.personal.spring_prac.validation.controller;
+
+
+import com.personal.spring_prac.validation.dto.Member;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class ValController {
+
+    // 입력받는 파라미터 앞에 @Valid 를 붙혀 검증을 한다. 양식에 맞지 않는경우 예외가 터지는데
+    // BindingResult 를 이용하면 잘못된 값이 안에 들어오게 된
+    @PostMapping("/member")
+    public Object user(@Valid @RequestBody Member member, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+
+                System.out.println("field : " + field.getField());      // 어떤 field 에서
+                System.out.println(message);                            // 어떤 에러가 발생했는지
+            });
+        }
+
+        System.out.println(member);
+        return member;
+    }
+}
---
+++ src/main/java/com/personal/spring_prac/validation/dto/Member.java
@@ -0,0 +1,20 @@
+package com.personal.spring_prac.validation.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class Member {
+
+    private String name;
+    private int age;
+
+    @Email
+    private String email;
+
+    // 정규식 으로 핸드폰 번호 양식 설정, message 로 양식과 맞지 않을경우 표시할 메세지를 설정 할 수 있음
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호 입력 양식을 확인해 주세요")
+    private String phoneNumber;
+}
